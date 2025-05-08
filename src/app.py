daed8729a622fa6f35e020adc9bad87d7919433a
Helen Zhou <helenz@helens-mbp.home>
2025-05-08 17:46:15 +0100

    Create a canvas where you can freedraw on a black background

--- src/app.py
+++ src/app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
-st.write("Hello world, starting out with streamlit for the first time!")
+# Create a canvas component
+canvas_result = st_canvas(
+    stroke_width=20,
+    stroke_color="#fff",
+    background_color="#000",
+    update_streamlit=True,
+    height=280,
+    width=280,
+    drawing_mode="freedraw",
+    point_display_radius=0,
+    key="canvas",
+)
