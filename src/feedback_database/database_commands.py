8671f9e70fef4ea87d211992fc0e0e59e7c64bed
Helen Zhou <helenz@helens-mbp.home>
2025-05-09 16:33:40 +0100

    Update SQL commands to create a feedback database to contain current primary key auto generated id, predicted digit, true digit, confidence percentage and timestamp of entry

--- src/feedback_database/database_commands.py
+++ src/feedback_database/database_commands.py
@@ -2,7 +2,6 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-# Connect to your postgres DB
 load_dotenv()
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
@@ -10,21 +9,23 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 conn = psycopg2.connect(f"user={POSTGRES_USERNAME} password={POSTGRES_PASSWORD} port={DB_PORT} host={DB_HOST}")
 
-# Open a cursor to perform database operations
 cur = conn.cursor()
-
-# Execute a query
-cur.execute("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name VARCHAR(100))")
-cur.execute("INSERT INTO test_table (name) VALUES ('John Doe');")
-
-# Retrieve query results
-cur.execute("SELECT * FROM test_table")
-
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS feedback (
+                id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                predicted_digit BIGINT,
+                true_digit BIGINT,
+                conf_percent FLOAT,
+                timestamp TIMESTAMP
+            )
+            """)
+cur.execute("INSERT INTO feedback (predicted_digit, true_digit, conf_percent, timestamp) VALUES ('1', '4', '90.0', NOW());")
+cur.execute("SELECT * FROM feedback")
 records = cur.fetchall()
 for record in records:
     print(record)
 
-cur.execute("DROP TABLE test_table")
+cur.execute("DROP TABLE feedback")
 
 conn.commit()
 conn.close()
