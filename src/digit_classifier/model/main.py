53507176a88d363443f22d3a3d4627a9725dd1c3
Helen Zhou <helenz@helens-mbp.home>
2025-05-14 14:16:27 +0100

    Fix bug where model kept predicting digit 5, since uint needed reshaping back to (280, 280, 4) after being sent from frontend

--- src/digit_classifier/model/main.py
+++ src/digit_classifier/model/main.py
@@ -10,7 +10,7 @@ model = init_model()
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
       content = await file.read()
-      data = numpy.frombuffer(content, dtype=numpy.uint8)
+      data = numpy.frombuffer(content, dtype=numpy.uint8).reshape((280, 280, 4))
       predicted_digit, conf_percent = predict_digit(data, model)
       return {
             "predicted_digit": predicted_digit,
@@ -18,5 +18,5 @@ async def predict(file: UploadFile = File(...)):
       }
 
 @app.get("/healthcheck")
-async def hello():
+async def healthcheck():
       return "The model API is up and running"
